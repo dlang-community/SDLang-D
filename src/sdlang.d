e0e8963ba10859adc49c76f7aac896541ef128b7
Nick Sabalausky <Abscissa>
2012-05-02 18:10:42 -0400

    Initial commit.

---
+++ src/sdlang.d
@@ -0,0 +1,28 @@
+/// SDLang-D
+/// Written in the D programming language.
+
+/++
+Library for parsing SDL (Simple Declarative Language).
+SDL: http://sdl.ikayzo.org/display/SDL/Language+Guide
+
+Author:
+$(WEB www.semitwist.com, Nick Sabalausky)
+
+This should work with DMD 2.059 and up.
+
+To compile manually:
+	rdmd --build-only -Isrc -ofbin/sdlang [dmd/rdmd options] src/sdlang.d
+
+You can also compile with stbuild (part of SemiTwist D Tools):
+	semitwist-stbuild all [release|debug|all]
+
+	Installation instructions for SemiTwist D Tools are here:
+		http://semitwist.com/goldie/Start/Install/
++/
+
+module sdlang;
+
+int main(string[] args)
+{
+	return 0;
+}
