f9ddbd79c148d21c25190bebfd83748f6b505b3b
Nick Sabalausky <Abscissa>
2013-02-28 03:09:47 -0500

    build-docs: Fix exclusion lists.

--- build-docs.bat
+++ build-docs.bat
@@ -1,4 +1,4 @@
 @echo off
 rdmd --build-only -lib -Isrc -D -X -Xfdocs/docs.json --force src/sdlang.d
-ddox filter docs/docs.json --ex sdlang_impl.lexer --ex sdlang_impl.symbol --min-protection Public
+ddox filter docs/docs.json --ex sdlang_.lexer --ex sdlang_.symbol --min-protection Public
 ddox generate-html docs/docs.json docs
