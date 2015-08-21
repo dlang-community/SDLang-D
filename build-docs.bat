d08d0704158f9c333f22d704419bb0fa1a70a31e
Nick Sabalausky <Abscissa@users.noreply.github.com>
2015-08-21 13:37:34 -0400

    Get rid of junk when building docs.

--- build-docs.bat
+++ build-docs.bat
@@ -1,4 +1,6 @@
 @echo off
-rdmd --build-only -c -Isrc -I../libInputVisitor -D -X -Xfdocs/docs.json --force src/sdlang/package.d
+rdmd --build-only -c -Isrc -I../libInputVisitor -Dddocs_tmp -X -Xfdocs/docs.json --force src/sdlang/package.d
+rmdir /S /Q docs_tmp > NUL 2> NUL
+del src\sdlang\package.exe
 ddox filter docs/docs.json --ex sdlang.lexer --ex sdlang.symbol --ex libInputVisitor --min-protection Public
 ddox generate-html docs/docs.json docs/public --navigation-type=ModuleTree --override-macros=ddoc/macros.ddoc
